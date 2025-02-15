138b31b42b8795a769f6d4e75c05c010c8eae885
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2025-02-15 19:43:43 +0900

    chore: defaultLanguage tsx

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -136,6 +136,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+      defaultLanguage: 'tsx',
     },
     plugins: [
       async function myPlugin(context, options) {
